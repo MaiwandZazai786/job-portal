92c9a9408e5d72b6a17859dd0e64f330cc55d297
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-08-23 01:13:15 +0200

    fav icon added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 </head>
 
 <body class="h-full">
