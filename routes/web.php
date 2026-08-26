6d60d5152ba13edf059e55c000812e1f74cff475
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-08-26 17:13:42 +0200

    jobs duplication removed

--- routes/web.php
+++ routes/web.php
@@ -3,58 +3,39 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Teacher',
+        'salary'    => '25,000',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary'    => '125,000',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Director',
+        'salary'    => '50,000',
+    ],
+
+];
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
-    return view('jobs', [
-
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Teacher',
-                'salary'    => '25,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary'    => '125,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Director',
-                'salary'    => '50,000',
-            ],
-
-        ]
-    ]);
+Route::get('/jobs', function () use ($jobs) {
+    return view('jobs', compact(['jobs']));
 });
 
 Route::get('contact', function () {
     return view('contact');
 });
 
-Route::get('job/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Teacher',
-            'salary'    => '25,000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary'    => '125,000',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Director',
-            'salary'    => '50,000',
-        ],
-
-    ];
+Route::get('job/{id}', function ($id) use ($jobs) {
 
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
