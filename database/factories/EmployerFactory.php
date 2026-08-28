099bc6f966fc10f10e816a4536292f444fb891f4
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-08-29 00:00:57 +0200

    employer changed to a company in factory

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -18,7 +18,7 @@ class EmployerFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name()
+            'name' => fake()->company()
         ];
     }
 }
