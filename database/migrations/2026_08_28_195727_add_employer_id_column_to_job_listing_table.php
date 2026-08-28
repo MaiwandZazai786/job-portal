f92fce74c12221de4d95597419a0ea9041935630
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-08-28 22:29:14 +0200

    employer id set to a default null in job listing table

--- database/migrations/2026_08_28_195727_add_employer_id_column_to_job_listing_table.php
+++ database/migrations/2026_08_28_195727_add_employer_id_column_to_job_listing_table.php
@@ -11,7 +11,9 @@ return new class extends Migration
     {
         Schema::table('job_listing', function (Blueprint $table) {
             $table->foreignId('employer_id')
-                ->constrained('employers', 'employer_id');
+                ->nullable()
+                ->constrained('employers', 'id')
+            ;
         });
     }
 
