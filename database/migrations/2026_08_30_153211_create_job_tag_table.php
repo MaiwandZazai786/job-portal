bc81e70214973554fb6da4bbaf0066b190be36af
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-08-30 17:42:37 +0200

    cascade on delete for both ids in pivot table

--- database/migrations/2026_08_30_153211_create_job_tag_table.php
+++ database/migrations/2026_08_30_153211_create_job_tag_table.php
@@ -6,24 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignId('job_listing_id')
-                ->constrained('job_listing', 'id');
+                ->constrained('job_listing', 'id')
+                ->cascadeOnDelete();
             $table->foreignId('tag_id')
-                ->constrained('tags', 'id');
+                ->constrained('tags', 'id')
+                ->cascadeOnDelete();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('job_tag');
