28fc72f4b59cef3712917cfea9646cc68d0e86f2
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-09-05 00:43:43 +0200

    client side validation for fields to be required

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,7 +16,8 @@
                             <div
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input id="title" type="text" name="title" placeholder="Software Engineer"
-                                    class="block min-w-0 grow bg-white py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                                    class="block min-w-0 grow bg-white py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    required />
 
                             </div>
                             <span class="text-red-600 text-sm/6">
@@ -33,7 +34,8 @@
                             <div
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input id="salary" type="text" name="salary" placeholder="$100,000 per year."
-                                    class="block min-w-0 grow bg-white py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                                    class="block min-w-0 grow bg-white py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    required />
                             </div>
                             <span class="text-red-600 text-sm/6">
                                 @error('salary')
