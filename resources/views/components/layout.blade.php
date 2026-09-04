5777147d78ab2dc39e7bae8388fb722659cdbf40
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-09-05 00:32:34 +0200

    text fixed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -109,7 +109,7 @@
         <header class="relative bg-white shadow-sm">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:items-center sm:justify-between">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
-                <x-create-button href="/jobs/create">Create New Job</x-create-button>
+                <x-create-button href="/jobs/create">Create Job</x-create-button>
             </div>
         </header>
         <main>
