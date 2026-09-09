d434db342fd843fe65cac061d2bd73800c9dfa84
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-09-09 23:28:31 +0200

    all job routes moved to one resource

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,5 @@ Route::view('/', 'home');
 // Contact page
 Route::view('contact', 'contact');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}', 'show');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+// Jobs resource routes 
+Route::resource('jobs', JobController::class);
