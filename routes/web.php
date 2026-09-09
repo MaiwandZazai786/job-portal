3f986707e354d73a07905cc277b79e3bf7bf8f30
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-09-09 23:22:32 +0200

    Job Routes Grouped

--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,12 @@ Route::view('/', 'home');
 // Contact page
 Route::view('contact', 'contact');
 
-//Jobs
-Route::get('/jobs', [JobController::class, 'index']);
-
-//Create Job
-Route::get('/jobs/create', [JobController::class, 'create']);
-
-//Show Job
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-
-//Store Job
-Route::post('/jobs', [JobController::class, 'store']);
-
-//Edit Job
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
-//Update Job
-Route::PATCH('/jobs/{job}', [JobController::class, 'update']);
-
-//Delete Job
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}', 'show');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
