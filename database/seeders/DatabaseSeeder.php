abcfa0637da970d537525400ac48492f4503a219
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2026-09-13 15:53:19 +0200

    user id added to employers table for creating the relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(100)->create();
         Employer::factory(50)->create();
         Job::factory(100)->create();
     }
